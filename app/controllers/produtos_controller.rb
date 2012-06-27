c212b1177b4273b4861877b2a207e33eac990d18
rafaelvinicio <rafaelvinicio@gmail.com>
2012-06-27 20:44:49 -0300

    primeiro commit

---
+++ app/controllers/produtos_controller.rb
@@ -0,0 +1,26 @@
+class ProdutosController <
+	ApplicationController
+
+	def index
+		@produtos = Produto.all
+	end
+
+    def show
+    	@produto = Produto.find(params[:id])
+    end
+    
+    def new
+    	@produto = Produto.new
+    end
+
+    def create
+    	@produto = Produto.new(params[:produto])
+    	 if @produto.save
+    	 	redirect_to produtos_path
+    	 else
+    	 	render :new
+    	 end
+   	end
+
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Loja::Application.config.secret_token = '4f8e2d2eaea180f6559c9045bdec75669f01d338c38b6194d5fb2745de855f4ed80c59bb2388a88909a20755a28c260a5685350a40eb2dfec2c02a112b5f794b'
